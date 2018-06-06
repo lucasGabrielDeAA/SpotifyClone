1b9ed5abe315648a807f87114b18525416860353
lucasGabrielDeAA <lg.technus@gmail.com>
2018-06-05 21:56:44 -0300

    Styling SongItem's component

--- src/components/SongItem/index.js
+++ src/components/SongItem/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
 
-import { View } from 'react-native';
+import { View, TouchableOpacity, Text } from 'react-native';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import PropTypes from 'prop-types';
 
 import styles from './styles';
 
-const SongItem = () => (
-  <View />
+const SongItem = ({ song }) => (
+  <TouchableOpacity style={styles.container} onPress={() => {}}>
+    <View style={styles.info}>
+      <Text style={styles.title}>{song.title}</Text>
+      <Text style={styles.author}>{song.author}</Text>
+    </View>
+    <Icon name="play-circle-outline" size={24} style={styles.play} />
+  </TouchableOpacity>
 );
+
+SongItem.propTtpes = {
+  song: PropTypes.shape({
+    title: PropTypes.string,
+    author: PropTypes.string,
+  }).isRequired,
+};
 
 export default SongItem;
--- src/components/SongItem/styles.js
+++ src/components/SongItem/styles.js
@@ -1,5 +1,27 @@
 import { StyleSheet } from 'react-native';
+import { colors, metrics } from 'styles';
 
 export default StyleSheet.create({
-
+  container: {
+    marginTop: metrics.basePadding,
+    marginHorizontal: metrics.basePadding,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  info: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 15,
+    color: colors.white,
+    fontWeight: 'bold',
+  },
+  author: {
+    fontSize: 14,
+    color: colors.dark,
+    marginTop: 3,
+  },
+  play: {
+    color: colors.regular,
+  },
 });
